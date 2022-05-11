e3620ca061fc6be3e6f5000f1fe25e16f49e6c20
Tamir Segal <tamseg56@gmail.com>
2022-05-12 01:13:48 +0300

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { calculate } = require("./calculate");
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.get('/calc', (req, res) => {
     let value1 = parseInt(req.query.value1);
